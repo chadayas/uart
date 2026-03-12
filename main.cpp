6a7eef86d537ce83c49efcd417552a038a07c873
chadayas <cgassoya@gmail.com>
2026-03-12 16:03:42 -0400

    start recieve func

--- main.cpp
+++ main.cpp
@@ -80,14 +80,12 @@ void open_USART_config(){
 	*GPIOA_AFRL |= (1 << 13); 
 	*GPIOA_AFRL |= (1 << 12);
 
+	// enable USART	
+	*USART2_CR1 |= (1 << 13);
 
 };
 
 void start_transmission(){
-	// enable USART	
-	*USART2_CR1 |= (1 << 13);
-	*USART2_CR1 |= (1 << 12);
-	
 	// setting baud rate at 115200	
 	*USART2_BRR = (8 << 4) | 11; 
 	
@@ -95,22 +93,25 @@ void start_transmission(){
 	*USART2_CR1 |= (1 << 3); 
 	// send T over transmission, T for true	
 	*USART2_DR = 'T';
+
+	// wait untill hardware sets TXE bit to 1 indicating 
+	// DR is empty, then fill with our char T
+	while(!(*USART2_SR & (1 << 7))) 
+		*USART2_DR = 'T';
+
 
 }
 
 void start_recieve(){
-	// enable USART	
-	*USART2_CR1 |= (1 << 13);
-	*USART2_CR1 |= (1 << 12);
-	
 	// setting baud rate at 115200	
 	*USART2_BRR = (8 << 4) | 11; 
 	
-	// enable the transmitter
-	*USART2_CR1 |= (1 << 3); 
-	// send T over transmission, T for true	
-	*USART2_DR = 'T';
+	// enable the recieve 
+	*USART2_CR1 |= (1 << 2); 
+	while(!(*USART2_SR & (1 << 5)))
+		uint8_t buf = *USART2_DR;
 
+	*USART2_CR1 |= (1 << 2);
 }
 
 int main()
