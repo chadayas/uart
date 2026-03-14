903c8d9b67c4c02b8f43939362e23dea5ceace5d
chadayas <cgassoya@gmail.com>
2026-03-14 19:35:00 -0400

    uart send func having fun writing to serial console

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 
 #include<cstdint>
 
-#define USART2 0x40004400 
-#define RCC 0x40023800
-#define GPIOA_BASE 0x40020000
+#define USART2_BASE 0x40004400
+#define RCC_BASE    0x40023800
+#define GPIOA_BASE  0x40020000
+
 #define GPIOA_MODER reinterpret_cast<volatile uint32_t*>(GPIOA_BASE + 0x00)
+#define GPIOA_AFRL  reinterpret_cast<volatile uint32_t*>(GPIOA_BASE + 0x20)
 
-#define RCC_AHB1ENR reinterpret_cast<volatile uint32_t*>(RCC + 0x30)
-#define RCC_APB1ENR reinterpret_cast<volatile uint32_t*>(RCC + 0x40)
+#define RCC_AHB1ENR reinterpret_cast<volatile uint32_t*>(RCC_BASE + 0x30)
+#define RCC_APB1ENR reinterpret_cast<volatile uint32_t*>(RCC_BASE + 0x40)
 
-#define GPIOA_AFRL reinterpret_cast<volatile uint32_t*>(GPIOA_BASE + 0x20)
+#define USART2_SR  reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x00)
+#define USART2_DR  reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x04)
+#define USART2_BRR reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x08)
+#define USART2_CR1 reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x0C)
+#define USART2_CR2 reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x10)
 
-#define USART2_SR reinterpret_cast<volatile uint32_t*>(USART2 + 0x00)
-#define USART2_CR1 reinterpret_cast<volatile uint32_t*>(USART2 + 0x0c)
-#define USART2_CR2 reinterpret_cast<volatile uint32_t*>(USART2 + 0x10)
-#define USART2_BRR reinterpret_cast<volatile uint32_t*>(USART2 + 0x08)
-#define USART2_DR reinterpret_cast<volatile uint32_t*>(USART2 + 0x04)
+void uart_send_string(const char* msg){
+	while(*msg){	
+		while(!(*USART2_SR & (1 << 7))); 
+		*USART2_DR = *msg++;
+	}
+
+}
 
 void open_USART_config(){
 	// enable clock for bus for GPIOA and USART2	
@@ -52,11 +60,9 @@ void start_transmission(){
 	// wait untill hardware sets TXE bit to 1 indicating 
 	// DR is empty, then fill with our char x
 	// loop through char array for send	
+	
 	const char text[] = "\n SLATT SLATT SLATT";	
-	for(auto& x : text ){	
-		while(!(*USART2_SR & (1 << 7))); 
-		*USART2_DR = x;
-	}
+	uart_send_string(text);	
 
 }
 
@@ -67,6 +73,7 @@ void start_recieve(){
 	while(!(*USART2_SR & (1 << 5)));
 	uint8_t buf = *USART2_DR;
 }
+
 
 int main()
 {
