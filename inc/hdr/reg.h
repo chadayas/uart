2a8100dcf4244d5fef604067d2e85c6ed93a8a82
chadayas <cgassoya@gmail.com>
2026-04-04 16:24:13 -0400

    replacing #defines with const and constexpr

--- inc/hdr/reg.h
+++ inc/hdr/reg.h
@@ -5,27 +5,27 @@
 // pointers to represent a memory addr
 
 
-#define USART2_BASE 0x40004400
-#define RCC_BASE    0x40023800
-#define GPIOA_BASE  0x40020000
-#define FLASH_BASE  0x40023C00
+constexpr auto USART2_BASE {0x40004400};
+constexpr auto RCC_BASE    {0x40023800};
+constexpr auto GPIOA_BASE  {0x40020000};
+constexpr auto FLASH_BASE  {0x40023C00};
 
-#define AIRCR reinterpret_cast<volatile uint32_t*>(0xE000ED0C)
+auto const AIRCR {reinterpret_cast<volatile uint32_t*>(0xE000ED0C)};
 
-#define GPIOA_MODER reinterpret_cast<volatile uint32_t*>(GPIOA_BASE + 0x00)
-#define GPIOA_AFRL  reinterpret_cast<volatile uint32_t*>(GPIOA_BASE + 0x20)
+auto const GPIOA_MODER {reinterpret_cast<volatile uint32_t*>(GPIOA_BASE + 0x00)}; 
+auto const GPIOA_AFRL {reinterpret_cast<volatile uint32_t*>(GPIOA_BASE + 0x20)};
 
-#define RCC_AHB1ENR reinterpret_cast<volatile uint32_t*>(RCC_BASE + 0x30)
-#define RCC_APB1ENR reinterpret_cast<volatile uint32_t*>(RCC_BASE + 0x40)
+auto const RCC_AHB1ENR {reinterpret_cast<volatile uint32_t*>(RCC_BASE + 0x30)};
+auto const RCC_APB1ENR {reinterpret_cast<volatile uint32_t*>(RCC_BASE + 0x40)};
 
-#define USART2_SR  reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x00)
-#define USART2_DR  reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x04)
-#define USART2_BRR reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x08)
-#define USART2_CR1 reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x0C)
-#define USART2_CR2 reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x10)
+auto const USART2_SR {reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x00)};
+auto const USART2_DR {reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x04)};
+auto const USART2_BRR {reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x08)};
+auto const USART2_CR1 {reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x0C)};
+auto const USART2_CR2 {reinterpret_cast<volatile uint32_t*>(USART2_BASE + 0x10)};
 
-#define FLASH_KEYR reinterpret_cast<volatile uint32_t*>(FLASH_BASE + 0x04)
-#define FLASH_CR reinterpret_cast<volatile uint32_t*>(FLASH_BASE + 0x10)
-#define FLASH_ACR reinterpret_cast<volatile uint32_t*>(FLASH_BASE + 0x00)
-#define FLASH_SR reinterpret_cast<volatile uint32_t*>(FLASH_BASE + 0x0C)
+auto const FLASH_KEYR {reinterpret_cast<volatile uint32_t*>(FLASH_BASE + 0x04)};
+auto const FLASH_CR {reinterpret_cast<volatile uint32_t*>(FLASH_BASE + 0x10)};
+auto const FLASH_ACR {reinterpret_cast<volatile uint32_t*>(FLASH_BASE + 0x00)};
+auto const FLASH_SR {reinterpret_cast<volatile uint32_t*>(FLASH_BASE + 0x0C)};
 
